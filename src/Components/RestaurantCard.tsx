11b222d67beeff58484116dfadbb7f68e551e33e
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-08 14:21:14 +0100

    add paper div and buttonGroup

--- src/Components/RestaurantCard.tsx
+++ src/Components/RestaurantCard.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Box from "@mui/material/Box";
 import {Restaurants} from "../data/filter-data";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import {ButtonGroup, Paper} from "@mui/material";
 
 export const RestaurantCard = () => {
     const [cards] = React.useState(Restaurants);
@@ -22,14 +25,37 @@ export const RestaurantCard = () => {
                     cards.map(restaurant => {
                     return (
 
-                        <Grid item key={restaurant.id}>
-                            <Box component={"img"} sx={{
-                                height: 230,
-                                display: 'block',
-                                overflow: 'hidden',
-                                width: 320,
-                                borderRadius: 3,
-                            }} src={restaurant.image}></Box>
+                        <Grid item key={restaurant.id} >
+                            <Paper elevation={5} sx={{
+                                padding: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center'
+                            }}>
+                                <Box component={"img"} sx={{
+                                    height: 230,
+                                    display: 'block',
+                                    overflow: 'hidden',
+                                    width: '100%',
+                                    borderRadius: 3,
+                                }} src={restaurant.image}>
+                                </Box>
+                                <Box>
+                                    <Typography>
+                                        {restaurant.name}
+                                    </Typography>
+                                </Box>
+                                <Typography>
+                                    {restaurant.address}
+                                </Typography>
+                                <Box>
+                                    <ButtonGroup>
+                                        <Button>J'aime</Button>
+                                        <Button>Commentaires</Button>
+                                        <Button>Commander</Button>
+                                    </ButtonGroup>
+                                    </Box>
+                            </Paper>
                         </Grid>
 
                     )
