8e2f13f2a23084c76da9dcc75c3f0c84b6960ba8
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-11 18:26:56 +0100

    Add InfoRestaurant.tsx

--- src/data/filter-data.tsx
+++ src/data/filter-data.tsx
@@ -23,7 +23,7 @@ export const Restaurants = [
         image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         menu: [
             {
-                "plat": "Coq au vin",
+                "name": "Coq au vin",
                 "type": "Plat",
                 "prix": 15,
                 "description": "Un plat traditionnel français à base de poulet cuit lentement dans du vin rouge et de légumes."
