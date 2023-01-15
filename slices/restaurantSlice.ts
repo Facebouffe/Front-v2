9f8a85c24259765f5889360cb6a715bf70459918
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-15 13:01:50 +0100

    add route API next Restaurant

--- slices/restaurantSlice.ts
+++ slices/restaurantSlice.ts
@@ -1,6 +1,7 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import {RootState} from "../store";
 import axios from "axios";
+import {UserState} from "./userSlice";
 
 export interface RestaurantState {
     data: {
@@ -12,4 +13,16 @@ export interface RestaurantState {
     }
     pending: boolean,
     error: boolean,
+}
+
+const initialState: RestaurantState = {
+    data: {
+        id: "",
+        name: "",
+        address: "",
+        rate: 0,
+        like: 0
+    },
+    pending: false,
+    error: false,
 }
