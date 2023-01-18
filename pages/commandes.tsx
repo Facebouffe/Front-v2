3c6c3cc34fc138bc779bfc2d2733fb706839c805
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-18 22:33:23 +0100

    Clean code

--- pages/commandes.tsx
+++ pages/commandes.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import {Box, Divider, Grid, Typography} from "@mui/material";
+import {Box, Divider, Typography} from "@mui/material";
 import {Header} from "../src/Components/Header";
 import CommandeEnCours from "../src/Components/CommandeEnCours";
 import AnciennesCommandes from "../src/Components/AnciennesCommandes";
-import Calendar from "../src/Components/calendar";
-import FilterDate from "../src/Components/FilterDate";
 
 
 const Commandes = () => {
@@ -13,15 +11,11 @@ const Commandes = () => {
     return (
         <Box>
             <Header/>
-
-             {/*<FilterDate>*/}
-                <Calendar/>
             <Typography sx={{fontSize: {xs: 40, md: 70}, ml: 5}} variant={'h2'}>En cours</Typography>
             <CommandeEnCours/>
             <Divider sx={{fontWeight: 'bold'}} variant={'middle'} orientation="horizontal" flexItem/>
             <Typography sx={{fontSize: {xs: 40, md: 70}, ml: 5}} variant={'h2'}>Anciennes commandes</Typography>
             <AnciennesCommandes/>
-        </FilterDate>
         </Box>
     );
 };
