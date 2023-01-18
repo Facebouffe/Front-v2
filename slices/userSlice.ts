24b40e60909f7e36e6b879ccfba7205763548e81
Sheridan SHABANI <sheridan.shabani@outlook.fr>
2023-01-18 13:32:03 +0100

    test getUser API

--- slices/userSlice.ts
+++ slices/userSlice.ts
@@ -38,7 +38,7 @@ export const getUser = createAsyncThunk('user/fetchUser', async (
     userId: string,
     thunkAPI
 ) => {
-    const response = await axios.get(`http://localhost:3000/api/user/${userId}`)
+    const response = await axios.get(`http://34.140.97.216:80/users/${userId}`)
 
     return response.data
 })
