e5fdbed6bdefb4f0defa5f4baee185e153fccf83
Sheridan SHABANI <sheridan.shabani@outlook.fr>
2023-01-19 13:38:23 +0100

    Clean Code

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -8,69 +8,6 @@ const Faq = () => {
     return (
         <Box>
             <Header/>
-            <Box sx={{mt: 4, ml: 4, mr: 4}}>
-                <Typography sx={{mb: 2, fontWeight: 'bold'}} variant={'h3'}>Pour vous, clients </Typography>
-                <Typography sx={{mb: 2}} variant={'h4'}>Qui sommes-nous ? </Typography>
-                <Typography>FaceBouffe s'est donné pour mission de révolutionner la manière dont vous commandez vos
-                    plats. Nous nous sommes associés aux meilleurs restaurants, de la petite adresse locale aux
-                    franchises cultes, afin de vous livrer vos plats préférés sur le pas de votre porte.
-                    Choisissez parmi des milliers de suggestions !
-                </Typography>
-
-                <Typography sx={{mb: 2}} variant={'h4'}>Pour vous, livreurs partenaires </Typography>
-                <Typography sx={{mb: 2}}>Vous avez une question et souhaitez contacter FaceBouffe ? Cliquez ici
-                    pour entrer en contact
-                    avec nos équipes </Typography>
-                <Typography sx={{mb: 2}} variant={'h4'}>Pourquoi FaceBouffe ? </Typography>
-                <Typography sx={{mb: 2}}>FaceBouffe est une success story à l'anglaise. Après avoir quitté New
-                    York pour s'installer à Londres, notre fondateur a dû se rendre à l'évidence : il lui était
-                    quasiment impossible de se faire livrer des plats de qualité. Il s’est donc donné pour mission de
-                    créer un service permettant de rapprocher les clients et leurs restaurants préférés.
-                    Nous sommes maintenant présents dans plus de 100 villes du Royaume-Uni, plus de 600 développeurs et
-                    salariés travaillent dans notre siège à Londres et nous collaborons régulièrement avec plus 8 000
-                    restaurants et 15 000 coursiers.
-                </Typography>
-                <Typography sx={{mb: 2, fontWeight: 'bold'}} variant={'h3'}>Commander sur FaceBouffe </Typography>
-
-                <Typography variant={'h4'} sx={{mb: 2}}>Comment ça marche ?</Typography>
-                <Typography sx={{mb: 2}}>Vous pouvez commander soit sur le site, soit via l'application mobile
-                    FaceBouffe, disponible
-                    sur iOS et Android. Saisissez simplement votre code postal pour trouver tous les restaurants
-                    disponibles dans votre quartier, choisissez vos plats et passez votre commande.
-                    Une fois que le restaurant aura reçu et accepté votre commande, celle-ci sera préparée et
-                    soigneusement emballée. Une fois votre commande prête, un livreur partenaire viendra la chercher et
-                    vous l'apporter.
-                    Si vous voulez vous organiser de manière optimale, vous pouvez également passer votre commande
-                    jusqu'à 24 heures à l'avance et planifier une livraison à l'heure de votre choix.
-                </Typography>
-
-                <Typography sx={{mb: 2}} variant={'h4'}>Qui est responsable de votre commande ?</Typography>
-                <Typography sx={{mb: 2}}>En cas d’acceptation de votre commande par un partenaire, un contrat de vente
-                    est formé
-                    uniquement entre vous et le partenaire. FaceBouffe n'est pas partie à un tel contrat et n'assume
-                    aucune responsabilité ayant pour origine un tel contrat ou découlant de ce contrat de vente.
-                    FaceBouffe n’assume aucune responsabilité concernant les produits vendus par nos partenaires. Il
-                    n’est ni l’acheteur, ni le vendeur des produits des partenaires.
-
-                    Chaque partenaire est responsable de la vente des produits que vous commandez sur les applications.
-                    Le partenaire doit garantir les produits qu’il propose sur les applications conformément aux
-                    réglementations applicables.
-
-                    Par ailleurs, en cas d’achat de produits non-périssables auprès d’un partenaire, celui-ci est
-                    responsable de la fourniture des informations relatives à votre droit de rétractation, au retour des
-                    produits ainsi que toute autre obligation incombant au vendeur.
-
-                    Pour toute question, vous pouvez soumettre votre demande à hello@facebouffe.fr.
-                </Typography>
-
-                <Typography sx={{mb: 2}} variant={'h4'}>Quels types de restaurant sont présents sur FaceBouffe
-                    ?</Typography>
-                <Typography sx={{mb: 2}}>Nous sélectionnons avec attention les meilleurs restaurants de votre quartier.
-                    De l'authentique restaurant thaï, aux burgers les plus gourmets, en passant par la pizzeria la plus
-                    italienne du coin, FaceBouffe ne travaille qu'avec les meilleurs restaurants pour ravir les papilles
-                    les plus exigeantes.
-                </Typography>
-            </Box>
             <Bottom/>
         </Box>
     );
--- src/Components/Bottom.tsx
+++ src/Components/Bottom.tsx
@@ -7,12 +7,14 @@ import {useRouter} from "next/router";
 export const Bottom = () => {
     const router = useRouter()
 
-
     return (
         <Box sx={{
             ...dFlex,
-            minHeight: {xs:830,md:331},
-            backgroundColor: 'primary.main', display: 'flex', justifyContent: 'center',flexDirection: {xs:'column',md:'row'}
+            minHeight: {xs: 830, md: 331},
+            backgroundColor: 'primary.main',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: {xs: 'column', md: 'row'}
         }}>
             <Card elevation={6} sx={{
                 backgroundColor: 'primary.main',
@@ -25,10 +27,7 @@ export const Bottom = () => {
             }}>
                 <CardContent>
                     <Typography variant={'h6'} sx={{fontWeight: 'bold'}}> Découvrir FaceBouffe</Typography>
-                    <Typography sx={{mt: 2,cursor:'pointer'}}>A Propos</Typography>
-                    <Typography sx={{cursor:'pointer'}}>Nous rejoindre </Typography>
-                    <Typography sx={{cursor:'pointer'}}>Espace Press</Typography>
-                    <Typography sx={{cursor:'pointer'}}>Inscription restaurant</Typography>
+                    <Typography sx={{mt: 2}}>A Propos</Typography>
                 </CardContent>
             </Card>
             <Card elevation={6} sx={{
@@ -42,10 +41,9 @@ export const Bottom = () => {
             }}>
                 <CardContent>
                     <Typography variant={'h6'} sx={{fontWeight: 'bold'}}> Mentions légales</Typography>
-                     <Typography onClick={() => router.push('/mentionlegale')} sx={{mt:2,cursor:'pointer'}}> Mentions légales </Typography>
-                <Typography sx={{cursor:'pointer'}}>Confidentialité</Typography>
-                    <Typography sx={{cursor:'pointer'}}>Cookies</Typography>
-                    {/*<Typography sx={{cursor:'pointer'}}>Speak Up</Typography>*/}
+                    <Typography onClick={() => router.push('/mentionlegale')} sx={{mt: 2, cursor: 'pointer'}}> Mentions
+                        légales </Typography>
+                    <Typography>Confidentialité</Typography>
                 </CardContent>
             </Card>
             <Card elevation={6} sx={{
@@ -58,10 +56,10 @@ export const Bottom = () => {
                 color: 'white'
             }}>
                 <CardContent>
-                    <Typography variant={'h6'} sx={{fontWeight: 'bold'}} > Aide</Typography>
-                    <Typography onClick={() => router.push('/contact')} sx={{cursor:'pointer'}}>Nous contacter</Typography>
-                    <Typography onClick={() => router.push('/faq')} sx={{cursor:'pointer'}}>FAQ</Typography>
-                    {/*<Typography>Types de cuisine</Typography>*/}
+                    <Typography variant={'h6'} sx={{fontWeight: 'bold'}}> Aide</Typography>
+                    <Typography onClick={() => router.push('/contact')} sx={{mt:2, cursor: 'pointer'}}>Nous
+                        contacter</Typography>
+                    <Typography onClick={() => router.push('/faq')} sx={{cursor: 'pointer'}}>FAQ</Typography>
                 </CardContent>
             </Card>
             <Card elevation={6} sx={{
@@ -76,9 +74,11 @@ export const Bottom = () => {
                 <CardContent>
                     <Typography variant={'h6'} sx={{fontWeight: 'bold'}}> Gardez FaceBouffe dans votre
                         poche</Typography>
-                    <Box sx={{display:'flex', flexDirection:'column',m:5}}>
-                        <Box sx={{borderRadius: 3, width: 210,mb:5,cursor:'pointer'}} component={"img"} src={'./apple.png'}></Box>
-                        <Box sx={{borderRadius: 3, width: 210,cursor:'pointer'}} component={"img"} src={'./googleplay.png'}></Box>
+                    <Box sx={{display: 'flex', flexDirection: 'column', m: 5}}>
+                        <Box sx={{borderRadius: 3, width: 210, mb: 5, cursor: 'pointer'}} component={"img"}
+                             src={'./apple.png'}></Box>
+                        <Box sx={{borderRadius: 3, width: 210, cursor: 'pointer'}} component={"img"}
+                             src={'./googleplay.png'}></Box>
                     </Box>
                 </CardContent>
             </Card>
